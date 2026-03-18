f404432b955f6c59355f51dd81caabb43fcc7dd9
nhatminhalex <139727388+nhatminhalex@users.noreply.github.com>
2026-03-17 19:40:24 -0700

    acceleration

--- streamline.py
+++ streamline.py
@@ -6,3 +6,6 @@ class Calculations:
         h = distance_from_launch_pad_to_tracker/math.cos(angle_of_tracker)
         altitude = math.sqrt((h^2)-(distance_from_launch_pad_to_tracker^2))
         return altitude
+    def acceleration(velocityatt1,velocityatt2,):
+        """All in meters/second. Interval is 1 second"""
+        return (velocityatt1-velocityatt2)
